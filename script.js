25d4e8e8137a97b330de06c0305f30c6b39ee9d1
DESKTOP-EPU3TTH\Shakir <shakirali.mit@gmail.com>
2024-03-22 22:46:15 +0500

    Lazy image loading implemented...

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ const revealSection = function (entries, observer) {
   const [entry] = entries;
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry);
+  observer.unobserve(entry.target);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
@@ -143,7 +143,29 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
-//
+// Reveal Images.
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-source...
+  entry.target.src = entry.target.dataset.src;
+  //
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+const imgOberver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+imgTargets.forEach(img => imgOberver.observe(img));
+
 // const header = document.querySelector('.header');
 // const message = document.createElement('div');
 // message.classList.add('cookie-message');
